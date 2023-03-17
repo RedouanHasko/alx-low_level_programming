d3d891914d503807faebba26dc8494d23e3d9ea3
Redouan Elhaskouri <relhaskouri2@gmail.com>
2023-03-16 18:57:28 -0700

    Task #6 Size of Types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,15 +3,15 @@
 /**
  * main - Entry point
  *
- * Return : always 0 (Success) 
+ * Return: always 0 (Success) 
  */
 int main(void)
 {
 
-	printf("Size of a char: %lu byte(s)\n",(unsigned long)sizeof(char));
-	printf("Size of a int: %lu byte(s)\n",(unsigned long)sizeof(int));
-	printf("Size of a long int: %lu byte(s)\n",(unsigned long)sizeof(long));
-	printf("Size of a long long int: %lu byte(s)\n",(unsigned long)sizeof(long long));
-	printf("Size of a float: %lu byte(s)\n",(unsigned long)sizeof(float));
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(char));
+	printf("Size of a int: %lu byte(s)\n", (unsigned long)sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(long long int));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(float));
 	return (0);
 }
