25f72e94f242ef5b2756b8748916cd85e8fad6b3
Redouan Elhaskouri <relhaskouri2@gmail.com>
2023-03-16 19:04:05 -0700

    Task #6 Size of Types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,15 +3,20 @@
 /**
  * main - Entry point
  *
- * Return: always 0 (Success) 
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
+	int n;
+	long int ln;
+	long long int lln;
+	char c;
+	float flt;
 
-	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(char));
-	printf("Size of a int: %lu byte(s)\n", (unsigned long)sizeof(int));
-	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(long int));
-	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(long long int));
-	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(float));
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(n));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(ln));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(lln));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(flt));
 	return (0);
 }
