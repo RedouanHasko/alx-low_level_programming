bfa85a9a91e44a5cbacee0dbd36f52509821e5a6
Redouan Elhaskouri <relhaskouri2@gmail.com>
2023-03-18 13:12:04 -0700

    Task #0 test if number pos or neg or zero

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if(n > 0)
+	{
+		printf("The number is positive\n");
+	}
+	else if(n == 0)
+	{
+		printf("The number is zero\n");
+	}
+	else
+	{
+		printf("The number is negative\n");
+	}
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+#This is the README file for the 0x01. C - Variables, if, else, while Directory Projects and tasks
