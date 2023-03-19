cf945940d110839ace28a47998ffe55ee1c5be06
Redouan Elhaskouri <relhaskouri2@gmail.com>
2023-03-18 18:09:45 -0700

    Task #2 putch lowercase alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Zero (Success)
+ */
+int main (void)
+{
+	char x;
+	for( x='a' ; x <= 'z' ; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
