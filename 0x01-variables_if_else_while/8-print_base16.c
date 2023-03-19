df8a351f482cfb3045b64b741aed0d34ad7af8a3
Redouan Elhaskouri <relhaskouri2@gmail.com>
2023-03-18 20:20:38 -0700

    Task #8 Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	char c;
+
+	for (i = '0' ; i <= '9' ; i++)
+	{
+		putchar(i);
+	}
+	for (c = 'a' ; c <= 'f' ; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
