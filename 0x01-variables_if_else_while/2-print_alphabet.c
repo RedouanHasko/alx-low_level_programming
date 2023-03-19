c091cd6187f6549c0dd4231b331d60af7ffe4a92
Redouan Elhaskouri <relhaskouri2@gmail.com>
2023-03-18 18:16:05 -0700

    Task #2 fix betty style

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,13 +5,12 @@
  *
  * Return: Zero (Success)
  */
-int main (void)
+int main(void)
 {
 	char x;
-	for ( x = 'a' ; x <= 'z' ; x++ )
-	{
+	
+	for (x = 'a' ; x <= 'z' ; x++)
 		putchar(x);
-	}
 	putchar('\n');
 	return (0);
 }
