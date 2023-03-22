b9d00f022d8d5be6b825582683ef1ff5d003dbd2
Redouan Elhaskouri <relhaskouri2@gmail.com>
2023-03-21 19:27:41 -0700

    Task #10 add two integers and return result

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * @a: first integer to use in calcule
+ * @b: second integer to use in calcule
+ *
+ * Return: the result of a+b
+ */
+int add(int a, int b)
+{
+	int res;
+
+	res = a + b;
+	return (res);
+}
