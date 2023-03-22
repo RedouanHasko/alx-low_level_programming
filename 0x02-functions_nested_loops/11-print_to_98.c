c1f6e9804ffc53314f98db9bd01bc84a57466287
Redouan Elhaskouri <relhaskouri2@gmail.com>
2023-03-21 19:58:20 -0700

    task #11 from any number to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * print_to_98 - print all the numbers from the input number till 98
+ * @n: the input number
+ */
+void print_to_98(int n)
+{
+	int i = 0;
+
+	if (n < 98)
+	{
+		for (i = n ; i <= 98 ; i++)
+		{
+			printf("%d, ", i);
+		}
+	} else if (n > 98)
+	{
+		for (i = n ; i >= 98 ; i--)
+		{
+			printf("%d, ", i);
+		}
+	} else
+	{
+		printf("%d", n);
+	}
+}
