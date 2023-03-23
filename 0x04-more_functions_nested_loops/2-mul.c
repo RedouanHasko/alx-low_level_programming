381bd14af161e26c2467648e46dbbf71cabc1e7f
Redouan Elhaskouri <relhaskouri2@gmail.com>
2023-03-23 02:03:50 -0700

    Task #2 create func mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two intigers. 
+ * @a: the 1st int 
+ * @b: the 2nd int
+ * Return: the result of (a x b)
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
