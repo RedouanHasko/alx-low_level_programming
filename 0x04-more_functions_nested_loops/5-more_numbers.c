ce369eed37f9efbf694b6d0222956e6a537abc79
Redouan Elhaskouri <relhaskouri2@gmail.com>
2023-03-23 03:40:01 -0700

    Task #5 create func print 10 times from 0 to 14 using _putchar

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * more_numbers - print from 0 to 14 (10 times).
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0 ; i <= 10 ; i++)
+	{
+		for (j = 0 ; j <= 14 ; j++)
+		{
+			if (j < 10)
+			{
+				_putchar(j + '0');
+			}
+			else
+			{
+				_putchar(j / 10 + '0');
+				_putchar(j % 10 + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
