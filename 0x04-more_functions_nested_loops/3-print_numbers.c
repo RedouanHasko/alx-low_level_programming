2a37ffe6b54ff89c76d5612484985c20eb409ca2
Redouan Elhaskouri <relhaskouri2@gmail.com>
2023-03-23 02:09:55 -0700

    Task #3 create func print_numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_numbers - print from 0 to 9 usig _putchar func
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(i + '0');
+	}
+}
