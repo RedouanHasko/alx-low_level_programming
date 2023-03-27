b20bed00025f48ae4772272bb439f7d2153ca501
Redouan Elhaskouri <relhaskouri2@gmail.com>
2023-03-27 13:39:30 -0700

    update

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * swap_int - swap the value of an int to another 
+ * swap_int - swap the value of an int to another
  * @a: An int to swap with b
  * @b: The int to swap with a
  */
