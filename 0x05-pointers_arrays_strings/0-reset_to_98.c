8cca1124a1001f630ef3819117f2d20d7f2d5a46
Redouan Elhaskouri <relhaskouri2@gmail.com>
2023-03-27 13:03:06 -0700

    Task #0 and #1 - reset to 98 and swap

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * set_to_98 - take an int and update the value it point to 98
+ * @n: is the int pointer to update it value.
+ */
+void set_to_98(int *n)
+{
+	n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - swap the value of an int to another 
+ * @a: An int to swap with b
+ * @b: The int to swap with a
+ */
+void swap_int(int *a, int *b)
+{
+	int tmp;
+
+	tmp = a;
+	a = b;
+	b = tmp;
+}
