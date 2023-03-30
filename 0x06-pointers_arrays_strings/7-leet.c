f0563378571253e6454b835ecb30dd26dc732f2d
Redouan Elhaskouri <relhaskouri2@gmail.com>
2023-03-30 02:42:58 -0700

    Task #7 Encoding string into 1337

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string into 1337.
+ * Letters a and A should be replaced by 4
+ * Letters e and E should be replaced by 3
+ * Letters o and O should be replaced by 0
+ * Letters t and T should be replaced by 7
+ * Letters l and L should be replaced by 1
+ * @s: the input string.
+ * Return: the modified string.
+ */
+char *leet(char *s)
+{
+	int i, j = 0;
+	int upper[] = {65, 69, 79, 84, 76};
+	int lower[] = {97, 101, 111, 116, 108};
+	int numbers[] = {52, 51, 48, 55, 49};
+
+	while (*(s + j) != '\0')
+	{
+		for (i = 0 ; i < 5 ; i++)
+		{
+			if (*(s + j) == upper[i] || *(s + j) == lower[i])
+			{
+				*(s + j) = number[i];
+				break;
+			}
+		}
+		j++;
+	}
+	return (s);
+}
