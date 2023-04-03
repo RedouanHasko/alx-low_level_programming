3bdc3091c08401a2b9dc7a64f343ec5b00008c7e
Redouan Elhaskouri <relhaskouri2@gmail.com>
2023-04-03 06:13:44 -0700

    update

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,21 +1,21 @@
 #include "main.h"
 
 /**
- * _strspn - Locates a character in a string.
- * @s: This is the main C string to be scanned.
- * @accept: This is the string containing the list of characters to match in s.
- * Return: number of bytes in the initial segment of s
- * which consist only of bytes from accept
- */
+* _strspn - Locates a character in a string.
+* @s: This is the main C string to be scanned.
+* @accept: This is the string containing the list of characters to match in s.
+* Return: number of bytes in the initial segment of s
+* which consist only of bytes from accept
+*/
 unsigned int _strspn(char *s, char *accept)
 {
 	int i, j;
-        int n = 0;
+	int n = 0;
 	char *s1, *s2;
 
 	s1 = s;
 	s2 = accept;
-	
+
 	i = 0;
 	while (s1[i] != '\0')
 	{
