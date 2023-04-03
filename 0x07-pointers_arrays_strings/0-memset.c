de2f4396bac8e8cc15f3ee812a975f9745972e16
Redouan Elhaskouri <relhaskouri2@gmail.com>
2023-04-03 03:23:11 -0700

    Task #0 memset func

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memset - fills memory with a constant byte.
+ * @s: starting adress of memory to be filled..
+ * @b: value to be filled.
+ * @n: number of bytes to be filled.
+ *
+ * Return: pointer to the memory area s.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0 ; i < n ; i++)
+	{
+		*(s + i) = b;
+	}
+	return (s);
+}
