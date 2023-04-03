2cad0eb4281e9b43a85ae366c0c622a51915820c
Redouan Elhaskouri <relhaskouri2@gmail.com>
2023-04-03 04:51:33 -0700

    Task #2 strch func

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string.
+ * @s: the input string.
+ * @c: the character to locate.
+ * Return: a pointer to the 1st occurrence of the character in the string.
+ * or a 0 if the character is not found.
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		return (s);
+		s++;
+	}
+	return (0);
+}
