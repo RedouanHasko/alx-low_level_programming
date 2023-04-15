879f2b0b8206a09979fc6e5aae26775634d6c885
Redouan Elhaskouri <relhaskouri2@gmail.com>
2023-04-15 16:31:49 -0700

    update

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point calclate the sum of all natural numbers bellow 1024,
+ * that multiples of 3 and 5 .
+ * Return: 0 (Success).
+ */
+int main()
+{
+	int i, sum = 0;
+
+	for (i = 0; i < 1024 ; i++)
+	{
+		if ((i % 3) == 0 || (i % 5) == 0)
+		{
+			sum += i;
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
