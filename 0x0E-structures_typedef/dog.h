dbae614d097cfd6d5f0954f91cbfa9adbd933254
Redouan Elhaskouri <relhaskouri2@gmail.com>
2023-04-17 11:46:52 -0700

    Task #0 Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure of the elements of a dog.
+ * @name: the name of the dog.
+ * @age: the age of the dog.
+ * @owner: the name of the owner of that dog.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
