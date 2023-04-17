862d808f5c0d3588dbbe14a0b345b72e07a87073
Redouan Elhaskouri <relhaskouri2@gmail.com>
2023-04-17 14:49:16 -0700

    update

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,15 +12,15 @@ void print_dog(struct dog *d)
 	{
 		return;
 	}
-	if ((*d).name == NULL)
+	if (d->name == NULL)
 	{
-		(*d).name = "(nil)";
+		d->name = "(nil)";
 	}
-	if ((*d).owner == NULL)
+	if (d->owner == NULL)
 	{
-		(*d).owner = "(nil)";
+		d->owner = "(nil)";
 	}
-	printf("Name: %s\n", (*d).name);
-	printf("Age: %f\n", (*d).age);
-	printf("Name: %s\n", (*d).owner);
+	printf("Name: %s\n", d->name);
+	printf("Age: %f\n", d->age);
+	printf("Name: %s\n", d->owner);
 }
