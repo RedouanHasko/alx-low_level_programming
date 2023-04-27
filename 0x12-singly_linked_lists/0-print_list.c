bfa987a1a855777f4b13c1668acd428b739fe083
Redouan Elhaskouri <relhaskouri2@gmail.com>
2023-04-27 13:36:52 -0700

    Task #0 Print list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_list - prints all the elements of a list_t list.
+ * @h: the 1st node in the linked list 'head'.
+ *
+ * Return: the number of nodes in the linked list.
+ */
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[%d] %s\n", 0, "(nil)");
+		}
+		else
+		{
+			printf("[%u] %s\n", h->len, h->str);
+		}
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
