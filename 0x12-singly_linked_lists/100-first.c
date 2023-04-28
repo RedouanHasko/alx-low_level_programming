7f2da1a63e6feea4a82e2c30b949df71811a5bcc
Redouan Elhaskouri <relhaskouri2@gmail.com>
2023-04-27 17:36:43 -0700

    Task #5 func runs before the main func

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+
+/**
+ * _first - function that runs before the main function is executed.
+ */
+void _first(void)
+{
+	printf("You're beat! and yet, you must allow,\n
+	I bore my house upon my back!\n");
+}
