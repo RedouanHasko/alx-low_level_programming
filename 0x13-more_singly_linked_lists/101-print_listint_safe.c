e69e5f4396ab5780d90df688f60178d273bf2864
Redouan Elhaskouri <relhaskouri2@gmail.com>
2023-05-02 17:30:14 -0700

    update

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -8,13 +8,13 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t n1 = 0;
+	size_t number = 0;
 	long int f;
 
 	while (head)
 	{
 		f = head - head->next;
-		n1++;
+		number++;
 		printf("[%p] %d\n", (void *)head, head->n);
 		if (f > 0)
 		{
@@ -26,5 +26,5 @@ size_t print_listint_safe(const listint_t *head)
 			break;
 		}
 	}
-	return (n1);
+	return (number);
 }
