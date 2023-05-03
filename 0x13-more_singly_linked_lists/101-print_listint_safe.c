115e61d99a27f2b6807a7306933d9e3173df988d
Redouan Elhaskouri <relhaskouri2@gmail.com>
2023-05-02 17:55:58 -0700

    Update

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -9,17 +9,15 @@
 size_t print_listint_safe(const listint_t *head)
 {
 	size_t number = 0;
-	long int f;
+	long int dif;
 
 	while (head)
 	{
-		f = head - head->next;
+		dif = head - head->next;
 		number++;
 		printf("[%p] %d\n", (void *)head, head->n);
-		if (f > 0)
-		{
+		if (dif > 0)
 			head = head->next;
-		}
 		else
 		{
 			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
