f6048f05e2d092c4f7d38bd595e5fa84f888ed70
Redouan Elhaskouri <relhaskouri2@gmail.com>
2023-05-08 19:22:42 -0700

    Task #3 cp

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -8,7 +8,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#define BUF_SIZE 1024
+#define BUFFER_SIZE 1024
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
