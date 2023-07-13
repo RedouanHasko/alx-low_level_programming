b882653dd780c574cca84ce7d17e1c97a7d51a92
Redouan ELHASKOURI <relhaskouri2@gmail.com>
2023-07-13 14:12:55 -0700

    Updated

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -32,7 +32,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 	{
 		return (-1);
@@ -42,15 +42,10 @@ int create_file(const char *filename, char *text_content)
 		w = write(fd, text_content, _strlen(text_content));
 		if (w == -1)
 		{
-			close(fd);
 			return (-1);
 		}
-		close(fd);
-		return (1);
 	}
-	else
-	{
-		close(fd);
-		return (1);
-	}
+
+	close(fd);
+	return (1);
 }
