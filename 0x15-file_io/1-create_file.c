f3db85496ede2c94e5014fc3beff92f9c9df500a
Redouan ELHASKOURI <relhaskouri2@gmail.com>
2023-07-12 18:43:16 -0700

    task #1 updated

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,6 +1,23 @@
 #include "main.h"
 
 /**
+ * _strlen - counts the length of a given string.
+ * @str: the string to count it length.
+ *
+ * Return: the length of the string.
+ */
+int _strlen(char *str)
+{
+	int c = 0;
+
+	while (str[c] != '0')
+	{
+		c++;
+	}
+	return (c);
+}
+
+/**
  * create_file - function that creates a file.
  * @filename: the name of the created file.
  * @text_content: the content of the file.
@@ -15,8 +32,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRANC, 0600);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 	{
 		return (-1);
