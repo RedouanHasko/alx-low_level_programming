0aab0f919cb42d9c223b006042b5cbdd65f59665
Redouan ELHASKOURI <relhaskouri2@gmail.com>
2023-07-13 18:35:06 -0700

    Task #3

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,63 @@
+#include "main.h"
+
+/**
+ * main - the entry point.
+ * program that copies the content of a file to another file.
+ * @argc: the arguments counter.
+ * @argv: the array of the arguments.
+ *
+ * Return: a number.
+ */
+int main(int argc, char *argv[])
+{
+	int fdin, fdout, nbr, nbw;
+	char txt[BUFFER_SIZE];
+
+	if (argc != 3)
+	{
+	dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+	exit(97);
+	}
+	fdin = open(argv[1], O_RDONLY);
+	if (fdin == -1)
+	{
+	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+	exit(98);
+	}
+	fdout = open(argv[2], O_CREAT, O_WRONLY, O_TRUNC, 0664);
+	if (fdout == -1)
+	{
+	dprintf(STDERR_FILENO, "Error: C'ant write to %s\n", argv[2]);
+		exit(99);
+	}
+	while ((nbr = read(fdin, txt, BUFFER_SIZE)) > 0)
+	{
+		nbw = write(fdout, txt, nbr);
+		if (nbw == -1)
+		{
+		dprintf(STDERR_FILENO, "Error: c'ant write to %s\n", argv[2]);
+		exit(99);
+		}
+	}
+	if (nbr == -1)
+	{
+	dprintf(STDERR_FILENO, "Error: C'ant red from file %s\n", argv[1]);
+	exit(98);
+	}
+	cls_f(fdin);
+	cls_f(fdout);
+	return (0);
+}
+
+/**
+ * cls_f - close the file.
+ * @fd: the file descriptor.
+ */
+void cls_f(int fd)
+{
+	if (close(fd) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: C'ant close fd %i\n", fd);
+		exit(100);
+	}
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -10,6 +10,8 @@
 #include <sys/stat.h>
 #include <stddef.h>
 
+#define BUFFER_SIZE 1024
+
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
